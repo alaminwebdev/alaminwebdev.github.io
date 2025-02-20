caba3f7d337e1da35b9af87bca03f2b8d58d9d68
Md. Al Amin <mohammadalamin.dev@gmail.com>
2025-02-20 10:57:10 +0600

    modify skills and contact section

--- main.js
+++ main.js
@@ -94,21 +94,6 @@ for (let i = 0; i < filterBtn.length; i++) {
     })
 }
 
-// Enabling Contact Form
-
-const form = document.querySelector('[data-form]');
-const formInputs = document.querySelectorAll('[data-form-input]');
-const formBtn = document.querySelector('[data-form-btn]');
-
-for(let i = 0; i < formInputs.length; i++) {
-    formInputs[i].addEventListener('input', function () {
-        if(form.checkValidity()) {
-            formBtn.removeAttribute('disabled');
-        } else { 
-            formBtn.setAttribute('disabled', '');
-        }
-    })
-}
 
 // Enabling Page Navigation 
 
