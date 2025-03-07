ac8e577d071b840943da45ed54a48110b0dd9f45
Md. Al Amin <mohammadalamin.dev@gmail.com>
2025-03-07 22:35:02 +0600

    added new section - snapshot

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ const elementToggleFunc = function (elem) { elem.classList.toggle("active"); }
 const sidebar = document.querySelector("[data-sidebar]");
 const sidebarBtn = document.querySelector("[data-sidebar-btn]");
 
-sidebarBtn.addEventListener("click", function() {elementToggleFunc(sidebar); })
+sidebarBtn.addEventListener("click", function () { elementToggleFunc(sidebar); })
 
 //Activating Modal-testimonial
 
@@ -43,56 +43,56 @@ overlay.addEventListener('click', highlightsModalFunc);
 
 //Activating Filter Select and filtering options
 
-const select = document.querySelector('[data-select]');
-const selectItems = document.querySelectorAll('[data-select-item]');
-const selectValue = document.querySelector('[data-select-value]');
-const filterBtn = document.querySelectorAll('[data-filter-btn]');
+// const select = document.querySelector('[data-select]');
+// const selectItems = document.querySelectorAll('[data-select-item]');
+// const selectValue = document.querySelector('[data-select-value]');
+// const filterBtn = document.querySelectorAll('[data-filter-btn]');
 
-select.addEventListener('click', function () {elementToggleFunc(this); });
+// select.addEventListener('click', function () {elementToggleFunc(this); });
 
-for(let i = 0; i < selectItems.length; i++) {
-    selectItems[i].addEventListener('click', function() {
+// for(let i = 0; i < selectItems.length; i++) {
+//     selectItems[i].addEventListener('click', function() {
 
-        let selectedValue = this.innerText.toLowerCase();
-        selectValue.innerText = this.innerText;
-        elementToggleFunc(select);
-        filterFunc(selectedValue);
+//         let selectedValue = this.innerText.toLowerCase();
+//         selectValue.innerText = this.innerText;
+//         elementToggleFunc(select);
+//         filterFunc(selectedValue);
 
-    });
-}
+//     });
+// }
 
-const filterItems = document.querySelectorAll('[data-filter-item]');
+// const filterItems = document.querySelectorAll('[data-filter-item]');
 
-const filterFunc = function (selectedValue) {
-    for(let i = 0; i < filterItems.length; i++) {
-        if(selectedValue == "all") {
-            filterItems[i].classList.add('active');
-        } else if (selectedValue == filterItems[i].dataset.category) {
-            filterItems[i].classList.add('active');
-        } else {
-            filterItems[i].classList.remove('active');
-        }
-    }
-}
+// const filterFunc = function (selectedValue) {
+//     for(let i = 0; i < filterItems.length; i++) {
+//         if(selectedValue == "all") {
+//             filterItems[i].classList.add('active');
+//         } else if (selectedValue == filterItems[i].dataset.category) {
+//             filterItems[i].classList.add('active');
+//         } else {
+//             filterItems[i].classList.remove('active');
+//         }
+//     }
+// }
 
-//Enabling filter button for larger screens 
+// //Enabling filter button for larger screens 
 
-let lastClickedBtn = filterBtn[0];
+// let lastClickedBtn = filterBtn[0];
 
-for (let i = 0; i < filterBtn.length; i++) {
-    
-    filterBtn[i].addEventListener('click', function() {
+// for (let i = 0; i < filterBtn.length; i++) {
 
-        let selectedValue = this.innerText.toLowerCase();
-        selectValue.innerText = this.innerText;
-        filterFunc(selectedValue);
+//     filterBtn[i].addEventListener('click', function() {
 
-        lastClickedBtn.classList.remove('active');
-        this.classList.add('active');
-        lastClickedBtn = this;
+//         let selectedValue = this.innerText.toLowerCase();
+//         selectValue.innerText = this.innerText;
+//         filterFunc(selectedValue);
 
-    })
-}
+//         lastClickedBtn.classList.remove('active');
+//         this.classList.add('active');
+//         lastClickedBtn = this;
+
+//     })
+// }
 
 
 // Enabling Page Navigation 
@@ -100,18 +100,44 @@ for (let i = 0; i < filterBtn.length; i++) {
 const navigationLinks = document.querySelectorAll('[data-nav-link]');
 const pages = document.querySelectorAll('[data-page]');
 
-for(let i = 0; i < navigationLinks.length; i++) {
-    navigationLinks[i].addEventListener('click', function() {
-        
-        for(let i = 0; i < pages.length; i++) {
-            if(this.innerHTML.toLowerCase() == pages[i].dataset.page) {
+for (let i = 0; i < navigationLinks.length; i++) {
+    navigationLinks[i].addEventListener('click', function () {
+
+        for (let i = 0; i < pages.length; i++) {
+            if (this.innerHTML.toLowerCase() == pages[i].dataset.page) {
                 pages[i].classList.add('active');
                 navigationLinks[i].classList.add('active');
                 window.scrollTo(0, 0);
             } else {
                 pages[i].classList.remove('active');
-                navigationLinks[i]. classList.remove('active');
+                navigationLinks[i].classList.remove('active');
             }
         }
     });
 }
+
+// LIGHTBOX AREA
+const lightbox = document.createElement('div')
+const images = document.querySelectorAll('img:not(.profile)')
+lightbox.id = 'lightbox'
+document.body.appendChild(lightbox)
+
+images.forEach(image => {
+    image.addEventListener('click', e => {
+        lightbox.classList.add('active')
+        const img = document.createElement('img')
+        img.src = image.src
+        while (lightbox.firstChild) {
+            lightbox.removeChild(lightbox.firstChild)
+        }
+        lightbox.appendChild(img)
+        //  This blocks the background from scolling when lightbox is open - must set overflow in main css otherwise on the first time you click on the modal the page will scroll to the top
+        document.body.style.overflowY = 'hidden'
+    })
+})
+
+lightbox.addEventListener('click', e => {
+    if (e.target !== e.currentTarget) return
+    lightbox.classList.remove('active')
+    document.body.style.overflowY = 'scroll'
+})
